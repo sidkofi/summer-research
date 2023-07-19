a073fb2bdb03951d0272126e7de9f49bb2dbbcdf
sidkofi <staylor1@olin.edu>
2023-07-18 23:50:59 -0400

    add support vector visualization

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ import matplotlib.pyplot as plt
 from load_matrix_from_csv import load_matrix_from_csv
 
 # SET
-data_path = "/home/sidkofi/Documents/summer research/data"
-matrix_type = "400x2000"
+DATA_PATH = "/home/sidkofi/Documents/summer-research/data"
+MATRIX_TYPE = "14x70"
+REGULARIZATION = 1.0  # strength is inversely proportional to c, must be float
 
 # automatically get all user data
-featureFolder = os.path.join(data_path)
+featureFolder = os.path.join(DATA_PATH)
 subfolders = os.listdir(featureFolder)
 subfolders = [
     subfolder
@@ -34,28 +35,29 @@ sessions = [
 ]
 numPeople = len(person_ids)
 
-# initialize data and labels
-matrix_temp = matrix_type.replace("x", "")
-feature_vals = re.findall(r"\d+", matrix_type)
+# initialize empty data and labels
+feature_vals = re.findall(r"\d+", MATRIX_TYPE)
 vector_size = int(feature_vals[0]) * int(feature_vals[1])
+
 allData = np.empty((0, vector_size))
 allLabels = []
-idx = 0
+IDX = 0
+correctCounts = np.zeros(len(sessions))
 
 # load in each matrix
 for person_id in person_ids:
     for session_id in session_ids:
         sess = sessions[session_id - 1]
         file_path = os.path.join(
-            data_path,
+            DATA_PATH,
             str(person_id),
-            matrix_type,
+            MATRIX_TYPE,
             "matrices",
             sess
             + "."
             + str(person_id)
             + ".normLap.fb.coeffs.norm1.rt."
-            + matrix_type
+            + MATRIX_TYPE
             + ".csv",
         )
         data = load_matrix_from_csv(file_path)
@@ -67,18 +69,18 @@ for person_id in person_ids:
 
         allData = np.vstack((allData, featureVector))
         allLabels.append(sess)
-        idx += 1
+        IDX += 1
 
 allData = normalize(allData)
 allLabels = np.array(allLabels)
-# Begin model training
-# determine number of people and sample size
 numSamples = allData.shape[0]
-correctCounts = np.zeros(len(sessions))
+
+# Begin model training
 
 np.random.seed(123)  # random seed for reproducibility
-
-t = SVC(kernel="linear")
+t = SVC(
+    kernel="linear", C=REGULARIZATION
+)  # set kernel function to linear for coefficients
 
 # get labels from sessions
 taskLabels = [session.split("_")[1] for session in sessions]
@@ -92,7 +94,7 @@ for i in range(numPeople):
     testData = allData[testIndices, :]
     testLabels = allLabels[testIndices]
 
-    # Use the remaining data for training
+    # use the remaining data for training
     trainIndices = np.setdiff1d(np.arange(numSamples), testIndices)
     trainData = allData[trainIndices, :]
     trainLabels = allLabels[trainIndices]
@@ -100,6 +102,18 @@ for i in range(numPeople):
     # train and test model
     svmModel = t.fit(trainData, trainLabels)
     predictions = svmModel.predict(testData)
+
+    # get feature importance
+    featureImportances = np.abs(svmModel.coef_)
+
+    # get original labels for support vectors
+    support_vector_indices = svmModel.support_
+    support_vector_labels = allLabels[support_vector_indices]
+
+    # reshape feature importance to original size
+    featureImportanceImages = featureImportances.reshape(
+        -1, int(feature_vals[0]), int(feature_vals[1])
+    )
 
     # get overall accuracy
     accuracy = np.sum(predictions == testLabels) / len(testLabels)
@@ -112,8 +126,8 @@ for i in range(numPeople):
         task_correct = np.sum(taskPredictions == testLabels[taskIndices])
         task_accuracy = task_correct / len(taskPredictions)
         task_correct_counts[j] += task_correct
-        task_result = "Yes" if task_correct > 0 else "No"
-        print(f"Task: {session} - Correctly predicted: {task_result}")
+        TASK_RESULT = "Yes" if task_correct > 0 else "No"
+        print(f"Task: {session} - Correctly predicted: {TASK_RESULT}")
 
 # plot correctly predicted tasks
 plt.figure()
